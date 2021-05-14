a762eb51418b9d2ea681907d0881e83da6460aba
toyugo <toyugo>
2021-05-14 14:47:44 +0000

    semistandard

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,6 @@
 const $ = window.$;
 $(document).ready(() => {
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    console.log(data)
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
